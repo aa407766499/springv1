f15e3101db646772d837ef0db160e7ed1b1a2a8a
Huzi <aa407766499@outlook.com>
2019-08-13 14:58:12 +0800

    Initial commit

---
+++ src/main/java/com/study/MyDispatchServlet.java
@@ -0,0 +1,33 @@
+package com.study;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Huzi114
+ * @ClassName: MyDispatchServlet
+ * @Description:
+ * @date 2019/8/13 14:47
+ */
+public class MyDispatchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+}
---
+++ src/main/java/com/study/annotation/MyController.java
@@ -0,0 +1,16 @@
+package com.study.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Huzi114
+ * @ClassName: MyController
+ * @Description:
+ * @date 2019/8/13 14:54
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyController {
+    String value() default "";
+}
